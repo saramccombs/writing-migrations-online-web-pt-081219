ba0625874e158da85993b96f9791be2524e118b1
saramccombs <theSaraMcCombs@gmail.com>
2019-11-03 12:25:40 -0500

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,10 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+
+    def change
+        create_table :students do |t|
+            # primary key will be auto generated via active record
+            t.string :name
+        end
+    end
+
 end
